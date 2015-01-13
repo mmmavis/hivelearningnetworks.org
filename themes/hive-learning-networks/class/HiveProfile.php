497786043354347616a17f08641e9c35ed17bc27
Mavis Ou <mmmavisss@gmail.com>
2015-01-13 15:59:11 -0800

    Created HiveContact PHP class

--- themes/hive-learning-networks/class/HiveProfile.php
+++ themes/hive-learning-networks/class/HiveProfile.php
@@ -1,19 +1,13 @@
 <?php
 
 
-  class HiveProfile {
-    public $name;
-    public $mainWebsite;
-    public $logoUrl;
-    public $description;
-    public $links;
+  class HiveContact {
+    public $headshot;
+    public $info;
 
-    public function __construct($name, $mainWebsite, $logoUrl, $description, $links) {
-      $this->name = $name;
-      $this->mainWebsite = $mainWebsite;
-      $this->logoUrl = $logoUrl;
-      $this->description = $description;
-      $this->links = $links;
+    public function __construct($headshot, $info) {
+      $this->headshot = $headshot;
+      $this->info = $info;
       // echo 'constractor';
     }
 
@@ -27,7 +21,37 @@
       // echo '*******' . $this->$prop . '*******<br/>';
       return $this->$prop;
     }
+  }
 
+
+  class HiveProfile {
+    public $name;
+    public $mainWebsite;
+    public $logo;
+    public $description;
+    public $links;
+    public $contacts;
+
+    public function __construct($name, $mainWebsite, $logo, $description, $links, $contacts) {
+      $this->name = $name;
+      $this->mainWebsite = $mainWebsite;
+      $this->logo = $logo;
+      $this->description = $description;
+      $this->links = $links;
+      $this->contacts = $contacts;
+      // echo 'constractor';
+    }
+
+    public function setProperty($prop, $val) {
+      $this->$prop = $val;
+      // echo 'property = ' . $prop . '<br/>';
+      // echo '<br/>value = ' . $this->$prop . '<br/><br/>===<br/>';
+    }
+
+    public function getProperty($prop) {
+      // echo '*******' . $this->$prop . '*******<br/>';
+      return $this->$prop;
+    }
 
   }
 
--- themes/hive-learning-networks/hive-profiles.php
+++ themes/hive-learning-networks/hive-profiles.php
@@ -6,6 +6,9 @@
   if( $fields ) {
     $numAllFields = count($fields);
     $count = 0;
+    $contact1 = new HiveContact;
+    $contact2 = new HiveContact;
+    $contact3 = new HiveContact;
 
     $currentProfile;
     foreach( $fields as $field ) {
@@ -13,34 +16,58 @@
 
       $field_type = $field['type'];
       $field_name = $field['name'];
+      $field_name_split = explode("_", $field_name);
       $field_label = $field['label'];
       // echo '<br/> $field_label = ' . $field_label . ' <br/>';
+      // echo '$field_name = ' . $field_name . ' <br/>';
+      // echo 'value = ' . get_field($field_name) . '</br></br>';
 
       if ( $field_type === 'tab' ) {
         if ( !empty($currentProfile) ) {
+          // add contacts to profile
+          $currentProfile->setProperty('contacts',array($contact1, $contact2, $contact3));
           // push profile to $allProfiles array
           array_push($allProfiles, $currentProfile);
         }
         // create new profile
         $currentProfile = new HiveProfile;
         $currentProfile->setProperty('name', $field_label);
+        $contact1 = new HiveContact;
+        $contact2 = new HiveContact;
+        $contact3 = new HiveContact;
       }
 
-      if ( explode("_", $field_name)[0] === 'website' ) {
-        echo '==' . get_field($field_name);
+      if ( $field_name_split[0] === 'website' ) {
+        // echo '==' . get_field($field_name) . '== <br/>';
         $currentProfile->setProperty('mainWebsite', get_field($field_name));
       }
 
-      if ( explode("_", $field_name)[0] === 'logo' ) {
-        $currentProfile->setProperty('logoUrl', wp_make_link_relative(get_field($field_name)));
+      if ( $field_name_split[0] === 'logo' ) {
+        $currentProfile->setProperty('logo', wp_make_link_relative(get_field($field_name)));
       }
 
-      if ( explode("_", $field_name)[0] === 'description' ) {
+      if ( $field_name_split[0] === 'description' ) {
         $currentProfile->setProperty('description', get_field($field_name));
       }
 
-      if ( explode("_", $field_name)[0] === 'links' ) {
+      if ( $field_name_split[0] === 'links' ) {
         $currentProfile->setProperty('links', get_field($field_name));
+      }
+
+      if ( $field_name_split[0] === 'contact1' ) {
+        // echo 'contact 1 === <br/>' . get_field($field_name) . '<br/><br/>';
+        // the_field($field_name);
+        $contact1->setProperty($field_name_split[1], get_field($field_name));
+      }
+      if ( $field_name_split[0] === 'contact2' ) {
+        // echo 'contact 2 === <br/>' . get_field($field_name) . '<br/><br/>';
+        // the_field($field_name);
+        $contact2->setProperty($field_name_split[1], get_field($field_name));
+      }
+      if ( $field_name_split[0] === 'contact3' ) {
+        // echo 'contact 3 === <br/>' . get_field($field_name) . '<br/><br/>';
+        // the_field($field_name);
+        $contact3->setProperty($field_name_split[1], get_field($field_name));
       }
 
       // last field in this Field Group, also the last field of the last Hive Profile
@@ -51,7 +78,7 @@
 
     }
 
-    echo '<br/>============== $allProfiles number ===== ' . count($allProfiles);
+    echo '<br/> ============== $allProfiles number ============== ' . count($allProfiles) . '<br/>';
 
     foreach( $allProfiles as $profile ) {
       add_profile_to_page($profile);
@@ -59,12 +86,31 @@
   }
 
 
+  function build_contacts_html($contacts) {
+    $html = '';
+    $current_count = 0;
+    foreach($contacts as $contact) {
+      $current_count = $current_count+1;
+      if ($contact->headshot && $contact->info) {
+        $html .=
+          '<div class="row contact-card">' .
+            '<div class="col-md-4">' .
+              '<div class="contact-photo"><img src="' . $contact->headshot . '" /></div>' .
+            '</div>' .
+            '<div class="col-md-8">' . $contact->info . '</div>' .
+          '</div>';
+      }
+    }
+    return $html;
+  }
+
   function add_profile_to_page($profile) {
-    $name = $profile->getProperty("name");
-    $mainWebsite = $profile->getProperty("mainWebsite");
-    $logoUrl = $profile->getProperty("logoUrl");
-    $description = $profile->getProperty("description");
-    $links = $profile->getProperty("links");
+    $name = $profile->getProperty('name');
+    $mainWebsite = $profile->getProperty('mainWebsite');
+    $logo = $profile->getProperty('logo');
+    $description = $profile->getProperty('description');
+    $links = $profile->getProperty('links');
+    $contacts = $profile->getProperty('contacts');
     echo
       '<div class="container">' .
         '<div class="hive-profile" data-profile="' . $name . '">' .
@@ -80,7 +126,7 @@
                 '<div class="col-md-6">' .
                   '<div class="hive-logo">' .
                     '<a href="' . $mainWebsite . '">' .
-                      '<img src="' . $logoUrl . '" />' .
+                      '<img src="' . $logo . '" />' .
                     '</a>' .
                   '</div>' .
                 '</div>' .
@@ -94,7 +140,7 @@
             '</div>' .
             '<!-- right half (contacts) =========================== -->' .
             '<div class="col-md-4">' .
-
+              build_contacts_html($contacts) .
             '</div>' .
           '</div>' .
         '</div>' .
